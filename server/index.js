54c17514aa01d77af7c22e753f682d18ecd151ad
Harshul Jain <harshulgangwal02@gmail.com>
2023-10-02 00:53:54 +0530

    Added ProductModel and route

--- server/index.js
+++ server/index.js
@@ -3,10 +3,28 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const db = require('./db');
-
 const app = express();
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var corsOptions = {
+    origin: "http://localhost:3000"
+}
+app.use(cors(corsOptions));
+
+
+const productRouter = require('./routes/productRouter');
+
+
+
+app.use('/api/', productRouter);
+
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to MyCafe"});
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
