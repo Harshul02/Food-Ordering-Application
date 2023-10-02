4ebabdef26ffe14b0e74c46f5a50eae49b1102c5
Harshul Jain <harshulgangwal02@gmail.com>
2023-10-02 16:58:12 +0530

    Added Authentication

--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -12,7 +12,6 @@ export const ProductsPreview = () => {
 
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
           breakpoint: { max: 4000, min: 3000 },
           items: 5
         },
